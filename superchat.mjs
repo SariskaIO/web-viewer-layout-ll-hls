4294a5709a78deace09c88a330523005b79519bd
brajendrak00068 <brajendrak00068>
2024-04-02 05:11:07 +0530

    fixes and default ur;

--- superchat.mjs
+++ superchat.mjs
@@ -9,19 +9,54 @@ function generateRandomString(length) {
 
     return result;
 }
+var addedIds = []
+let socket = null;
+let channel =  null;
 
 async function startChatApp(channelName) {
-    console.log("start chat app", channelName);    
+    if (socket) {
+        socket.disconnect();
+    }
+
+    if (channel) {
+        channel.leave();
+    }
+
+    // Clear existing messages and online user lists from the DOM
+    if (document.getElementById("msg-list")){    
+        document.getElementById("msg-list").innerHTML = '';
+        const peopleListMobile = document.getElementById('people_online-list-mobile');      // online people list mobile
+        const peopleListDesktop = document.getElementById('people_online-list-desktop');   
+        peopleListMobile.innerHTML = '';
+        peopleListDesktop.innerHTML = '';
+    } 
+
+
+    // Reset variables
+    addedIds = [];
+
+
     let Socket  = window.Phoenix.Socket;
     // And connect to the path in "lib/chat_web/endpoint.ex". We pass the
     // token for authentication. Read below how it should be used.
     const getToken = async () => {
-        // Check if token exists in session storage
-        const storedToken = sessionStorage.getItem('token');
-        if (storedToken) {
-            return storedToken;
+        // Check if id and name exist in session storage
+        let id = sessionStorage.getItem('id');
+        let name = sessionStorage.getItem('name');
+        let token =  sessionStorage.getItem("token");            
+        if (token) {
+            return token;
         }
-        
+    
+        if (!id || !name) {
+            // If id or name doesn't exist, generate random strings
+            id = generateRandomString();
+            name = generateRandomString();
+    
+            // Store generated id and name in session storage
+            sessionStorage.setItem('id', id);
+            sessionStorage.setItem('name', name);
+        }
         // Token doesn't exist in session storage, fetch a new one
         const body = {
             method: "POST",
@@ -29,10 +64,10 @@ async function startChatApp(channelName) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                apiKey: "249202aabed00b41363794b526eee6927bd35cbc9bac36cd3edcaa",
+                apiKey: "27fd6f9296c71b4a2f2ad1f533e6b5c075c005bbdeac76923e",
                 user: {
-                    id: generateRandomString(),
-                    name: generateRandomString(),
+                    id,
+                    name,
                     email: "nick@gmail.com",
                     avatar: "https://test.com/user/profile.jpg",
                     moderator: true
@@ -44,11 +79,8 @@ async function startChatApp(channelName) {
             const response = await fetch("https://api.sariska.io/api/v1/misc/generate-token", body);
             if (response.ok) {
                 const json = await response.json();
-                const token = json.token;
-                
-                // Store token in session storage
-                sessionStorage.setItem('token', token);
-                
+                const token = json.token;    
+                sessionStorage.setItem("token", token);            
                 return token;
             } else {
                 console.log(response.status);
@@ -57,9 +89,8 @@ async function startChatApp(channelName) {
             console.log('error', error);
         }
     };
-    console.log({token:  await getToken()})
-    let socket = new Socket("wss://api.sariska.io/api/v1/messaging/websocket", {params: { token:  await getToken()}});
 
+    socket = new Socket("wss://api.sariska.io/api/v1/messaging/websocket", {params: { token:  await getToken()}});
     // Connect to the socket:
     socket.onOpen = () => {
         console.log("Socket opened", socket);
@@ -84,19 +115,16 @@ async function startChatApp(channelName) {
     /* INITIAL SETUP OF VARIABLES AND JOINING CHANNEL -------------- */
     const ul = document.getElementById('msg-list');                       // list of messages.
     const name = document.getElementById('name');                         // name of message sender
-    const msg = document.getElementById('msg');                           // message input field
+    const msg = document.getElementById('chat');                           // message input field
     const send = document.getElementById('send');                         // send button
     const peopleListMobile = document.getElementById('people_online-list-mobile');      // online people list mobile
     const peopleListDesktop = document.getElementById('people_online-list-desktop');      // online people list desktop
-
-    let channel = socket.channel(`chat:${channelName.toLowerCase()}`); // connect to chat "
-
+    channel = socket.channel(`chat:${channelName.toLowerCase()}`); // connect to chat "
     /* ONLINE people / PRESENCE FUNCTIONS -------------- */
     // This function will be probably caught when the person first enters the page
     channel.on('presence_state', function (payload) {
         // Array of objects with id and username
         const currentlyOnlinePeople = Object.entries(payload).map(elem => ({username: elem[0], id: elem[1].metas[0].phx_ref}));
-            
         updateOnlinePeopleList(currentlyOnlinePeople);
     });
     // Listening to presence events whenever a person leaves or joins
@@ -105,7 +133,6 @@ async function startChatApp(channelName) {
             // Array of objects with id and username
             const currentlyOnlinePeople = Object.entries(payload.joins).map(elem => ({username: elem[0], id: elem[1].metas[0].phx_ref}));
             const peopleThatLeft = Object.entries(payload.leaves).map(elem => ({username: elem[0], id: elem[1].metas[0].phx_ref}));
-
             updateOnlinePeopleList(currentlyOnlinePeople);
             removePeopleThatLeft(peopleThatLeft);
         }
@@ -120,22 +147,21 @@ async function startChatApp(channelName) {
         console.log(user);
         console.log(room);
     });
+
     channel.join()
     .receive("ok", ()=>console.log("Channel joined"))
     .receive("error", ()=>console.log("Failed to join"))
     .receive("timeout", () => console.log("Encountering network connectivity problems. Waiting for the connection to stabilize."))
-    var addedIds = new Set();
 
     function updateOnlinePeopleList(currentlyOnlinePeople) {
         // Set to keep track of added IDs    
         // Add joined people
         for (var i = currentlyOnlinePeople.length - 1; i >= 0; i--) {
-            console.log("currentlyOnlinePeople[i]", currentlyOnlinePeople[i]);
             const name = currentlyOnlinePeople[i].username;
-            const id = name + "-" + currentlyOnlinePeople[i].id;
-    
+            const id = currentlyOnlinePeople[i].id;
             // Check if the ID has already been added
-            if (!addedIds.has(id)) {
+            if (addedIds.indexOf(id) < 0 ) {
+                addedIds.push(id);
                 if (document.getElementById(name) == null) {
                     var liMobile = document.createElement("li"); // create new person list item DOM element for mobile
                     var liDesktop = document.createElement("li"); // create new person list item DOM element for desktop
@@ -149,7 +175,6 @@ async function startChatApp(channelName) {
                     peopleListDesktop.appendChild(liDesktop); // append to people list
     
                     // Add the ID to the set
-                    addedIds.add(id);
                 }
             }
         }
@@ -160,7 +185,7 @@ async function startChatApp(channelName) {
         // Remove people that left
         for (var i = peopleThatLeft.length - 1; i >= 0; i--) {
             const name = peopleThatLeft[i].name;
-            const id = name + "-" + peopleThatLeft[i].id;
+            const id = peopleThatLeft[i].id;
 
             const personThatLeftMobile = document.getElementById(id + '_mobile');
             const personThatLeftDesktop = document.getElementById(id +  '_desktop');
@@ -188,11 +213,32 @@ async function startChatApp(channelName) {
     
     // Send the message to the server on "shout" channel
     function sendMessage() {
-        channel.push('new_message', {        
-            content: msg.value,          // get message text (value) from msg input field.
+        let message =  msg.value
+        let content_type = "text"
+        if (document.getElementById("fileUrl").value) {
+            message = document.getElementById("fileUrl").value;
+            content_type = "file";
+        }
+        channel.push('new_message', {      
+            content_type,  
+            content: message,          // get message text (value) from msg input field.
         });
+        msg.value = ''; // This will clear the textarea value
 
-        msg.value = '';                // reset the message input field for next message.
+        // Temporarily disable textarea resizing
+        msg.style.resize = 'none';
+        if (document.getElementById("fileUrl").value) {
+            document.getElementById("fileUrl").value = "";
+            document.getElementById("attachedDocumentContainer").style.display = "none";
+            document.getElementById("attachedDocumentName").innerHTML = ""
+        }
+        // Move the cursor to the top of the textarea
+        msg.scrollTop = 0;
+        
+        // Re-enable textarea resizing after a short delay (e.g., 100 milliseconds)
+        setTimeout(function() {
+            msg.style.resize = 'auto';
+        }, 100);
     }
 
     // The page does not automatically scroll to show the latest message
@@ -209,19 +255,26 @@ async function startChatApp(channelName) {
 
         let firstCharacter  = payload.created_by_name[0];
         li.innerHTML = `
-        <div style="display: flex;justify-content: space-between;" class="flex flex-row w-[95%] mx-2 border-b-[1px] border-slate-300 py-2">
-            <div style="height: 30px;
-            width: 34px;
-            border-radius: 30px;
-            background: red;
-            margin-right: 5px;" class="thumbnail">${firstCharacter}</div>
+        <div style="display: block;justify-content: space-between;" class="flex flex-row w-[95%] mx-2 border-b-[1px] border-slate-300 py-2">
+        <div style="display: flex;margin-bottom: 10px;" id="user-container">
+            <div style="height: 30px; width: 34px; border-radius: 30px; background: red; margin-right: 5px;" class="thumbnail">${firstCharacter}</div>
             <div class="text-left w-2/5 font-semibold text-slate-800">
-            ${payload.created_by_name}
-            </div>
-            <div class="flex w-3/5 mx-1 grow">
-            ${payload.content}
+                ${payload.created_by_name}
             </div>
         </div>
+        <div style="max-width: 400px;text-align: left;margin-bottom: 10px;" class="flex w-3/5 mx-1 grow">
+            <!-- Conditional rendering -->
+            ${payload.content_type === 'file' ? 
+                (payload.content.toLowerCase().endsWith('.png') || payload.content.toLowerCase().endsWith('.jpg') || payload.content.toLowerCase().endsWith('.jpeg') || payload.content.toLowerCase().endsWith('.gif') ?
+                    `<a href="${payload.content}" download="filename">
+                        <img src="${payload.content}" alt="File Image">
+                     </a>` :
+                    `<a href="${payload.content}" download="filename">
+                        ${payload.content}
+                     </a>`) :
+                payload.content}
+        </div>
+    </div>
         `
         // Append to list
         ul.appendChild(li);
@@ -229,15 +282,15 @@ async function startChatApp(channelName) {
     }
 
     // Listen for the [Enter] keypress event to send a message:
-    msg.addEventListener('keypress', function (event) {
-        if (event.keyCode == 13 && msg.value.length > 0) { // don't sent empty msg.
+    msg && msg.addEventListener('keypress', function (event) {
+        if (event.keyCode == 13 && (msg.value.length > 0 || document.getElementById("fileUrl").value)) { // don't sent empty msg.
             sendMessage()
         }
     });
 
     // On "Send" button press
-    send.addEventListener('click', function (event) {
-        if (msg.value.length > 0) { // don't sent empty msg.
+    send && send.addEventListener('click', function (event) {
+        if (msg.value.length > 0 || document.getElementById("fileUrl").value) { // don't sent empty msg.
             sendMessage()
         }
     });
